1a5f089e4a903e7c5631e72eefe3f77293f7b5ac
r0zbeh <rozbeh.ardekani@gmail.com>
2023-09-20 16:23:32 +0330

    1

--- example/bot.py
+++ example/bot.py
@@ -10,14 +10,17 @@ from os import getenv
 # Define a few command handlers.
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_html(text="hello world!")
+    
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     chatid=update.message.chat.id
     text=update.message.text.split(' ')
+    await update.message.reply_html(text="1")
     data={"chatid":chatid,"amount":text[1]}
+    await update.message.reply_html(text="2")
     requests.post("http://ir.asmantarh.ir:5000/variz/",data=data,allow_redirects=False)
     
 
-    await update.message.reply_html(text=text)
+    await update.message.reply_html(text="3")
 
 async def bot_tele(text):
     # Create application
